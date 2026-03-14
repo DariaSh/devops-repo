475ef49ddc57e2ea46bc7736a48a28aafd680840
DariaSh <39339052+DariaSh@users.noreply.github.com>
2026-03-14 11:30:51 +0000

    Testing Build Trigger

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-model = {"title":  "Hello Build Trigger." }
-return \
-    render_template('index.html', \
-    model=model)
+    model = {"title":  "Hello Build Trigger." }
+    return \
+        render_template('index.html', \
+        model=model)
 
 if __name__ == "__main__":
 app.run(host='0.0.0.0', port=8080, \
